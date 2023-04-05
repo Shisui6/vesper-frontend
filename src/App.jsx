f762d54c01d8243b39b5b0524e90b0e78dac1dec
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-04-05 16:34:09 +0100

    feat(app): Create mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useIsAuthenticated } from 'react-auth-kit';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   const isAuthenticated = useIsAuthenticated();
-  const applyStyle = isAuthenticated() ? { marginLeft: '14.5rem' } : {};
+  const [windowDimension, setWindowDimension] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const margin = () => {
+    if (isAuthenticated() && location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/register') {
+      if (windowDimension > 768) {
+        return { marginLeft: '14.5rem' };
+      }
+
+      return { marginLeft: '4rem' };
+    }
+
+    return {};
+  };
+
+  const navbar = () => {
+    if (isAuthenticated() && location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/register') {
+      return <Navbar />;
+    }
+
+    return '';
+  };
 
   return (
     <>
-      {isAuthenticated() && <Navbar />}
+      {navbar()}
 
-      <div id="detail" style={applyStyle}>
+      <div id="detail" style={margin()}>
         <Outlet />
       </div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useSignOut } from 'react-auth-kit';
 import {
@@ -11,11 +12,64 @@ import {
 const Navbar = () => {
   const signOut = useSignOut();
   const navigate = useNavigate();
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const logout = () => {
     signOut();
     navigate('/');
   };
+
+  if (windowDimension < 768) {
+    return (
+      <nav>
+        <ul className="absolute top-0 bottom-0 shadow-lg py-3 rounded-r-xl mx-0 my-auto h-fit">
+          <li>
+            <NavLink to="cars" className="Navlink flex gap-2 p-3 rounded-tr-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+              <UilCarSideview />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="login" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+              <UilPlus />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="login" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+              <UilBook />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="login" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+              <UilPlusCircle />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="login" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+              <UilTrashAlt />
+            </NavLink>
+          </li>
+          <li className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100">
+            <button type="button" onClick={logout} className="flex items-center gap-2">
+              <UilSignout />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 
   return (
     <nav className="Navbar absolute left-0 h-screen w-52 shadow-lg flex flex-col justify-between py-10 px-4">
@@ -25,36 +79,36 @@ const Navbar = () => {
       </div>
       <ul>
         <li>
-          <NavLink to="/cars" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : '')}>
+          <NavLink to="cars" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilCarSideview />
             Cars
           </NavLink>
         </li>
         <li>
-          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilPlus />
             New Reservation
           </NavLink>
         </li>
         <li>
-          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilBook />
             My Reservations
           </NavLink>
         </li>
         <li>
-          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilPlusCircle />
             Add Car
           </NavLink>
         </li>
         <li>
-          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilTrashAlt />
             Delete Car
           </NavLink>
         </li>
-        <li className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white duration-100">
+        <li className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100">
           <button type="button" onClick={logout} className="flex items-center gap-2">
             <UilSignout />
             Logout
