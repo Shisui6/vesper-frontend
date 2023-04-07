9c85fc8aa7ca114d324b0f0663c5513a887b7a9c
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-07 15:09:23 +0430

    Create details page

---
+++ src/components/Details/carsDetails.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import Loader from '../Loader/Loader';
+import { AiOutlineRight } from 'react-icons/ai';
+
+import {
+    BsArrowRightCircleFill,
+    BsFillArrowLeftCircleFill,
+  } from 'react-icons/bs';
+  import { SlSettings } from 'react-icons/sl';
