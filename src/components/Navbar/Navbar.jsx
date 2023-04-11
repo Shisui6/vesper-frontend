74b27c4799779dc6d83122b9254e51828444a6f8
Usirusen Ikon <lexicon048@gmail.com>
2023-04-11 11:08:32 +0100

    Add link to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="login" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+            <NavLink to="deleteCar" className="Navlink flex gap-2 p-3 mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
               <UilTrashAlt />
             </NavLink>
           </li>
@@ -103,7 +103,7 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="login" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
+          <NavLink to="deleteCar" className="Navlink flex gap-2 p-3 rounded-xl mb-2 text-sm items-center cursor-pointer hover:text-white hover:bg-[#97bf0f] duration-100" style={({ isActive }) => (isActive ? { backgroundColor: '#97bf0f', color: '#fff' } : {})}>
             <UilTrashAlt />
             Delete Car
           </NavLink>
